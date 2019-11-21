71e86d2a448fea22b6d88c58ae0eb59e3cbb2d7e
Zuzana <wlachovska.zuzana@gmail.com>
2019-11-21 15:47:54 +0100

    pridane then pre hladanie sasanky

--- src/test/java/starter/stepdefinitions/SearchOnGoogleStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/SearchOnGoogleStepDefinitions.java
@@ -1,10 +1,15 @@
 package starter.stepdefinitions;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import net.thucydides.core.annotations.Steps;
 import starter.navigation.NavigateToGoogle;
 import starter.search.SearchFor;
+import starter.search.SearchResult;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static starter.matchers.TextMatcher.textOf;
 
 public class SearchOnGoogleStepDefinitions {
     @Steps
@@ -13,13 +18,22 @@ public class SearchOnGoogleStepDefinitions {
     @Steps
     SearchFor searchFor;
 
+    @Steps
+    SearchResult searchResult;
+
     @Given("im on Google.com")
     public void i_am_on_the_google() {
         navigateToGoogle.googleHomePage();
     }
 
     @When("he searches {string}")
-    public void heSearches(String term) {
+    public void searches_for_sasanka(String term) {
         searchFor.term(term);
+    }
+
+    @Then("all result titles should contain the word {string}")
+    public void all_result_titles_should_contain_the_word_sasanka(String expectedTerm) {
+        assertThat(searchResult.titles())
+                .allMatch(title -> textOf(title).containsIgnoringCase(expectedTerm));
     }
 }
--- src/test/resources/features/search/search_sasanka_google.feature
+++ src/test/resources/features/search/search_sasanka_google.feature
@@ -3,3 +3,4 @@ Feature: Search sasanka on Google
   Scenario: Searching for a term
     Given im on Google.com
     When he searches "sasanka"
+    Then all result titles should contain the word "Sasanka"
